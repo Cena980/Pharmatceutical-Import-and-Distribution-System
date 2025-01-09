286851792cc98b831fac43a35f543b8c98b9f440
Alisina Nazari <cena.nazari980@gmail.com>
2025-01-09 19:35:29 +0430

    date, customer id and cut id redundancy in sales insert has been fixed

--- Front End/php/insertsales.php
+++ Front End/php/insertsales.php
@@ -4,15 +4,19 @@
     $qnt = isset($_POST['qnt']) ? intval($_POST['qnt']) : 0;
     echo "Number of rows submitted: " . $qnt;
 
+    $Date0 = $_POST["Date_1"];
+    $Employee_Cut0 = $_POST["Cut_ID_1"];
+    $Location_ID0 = $_POST["Location_ID_1"];
+
     for ($i = 1; $i <= $qnt; $i++) {
         $Sale_ID = $_POST["Sale_ID_$i"] ?? null;
         $Drug_ID = $_POST["Drug_ID_$i"] ?? null;
-        $Date = $_POST["Date_$i"] ?? null;
+        $Date = $Date0;
         $Quantity = $_POST["Quantity_$i"] ?? null;
         $Discount = $_POST["Discount_$i"] ?? null;
         $Price = $_POST["Price_$i"] ?? null;
-        $Employee_Cut = $_POST["Cut_ID_$i"] ?? null;
-        $Location_ID = $_POST["Location_ID_$i"] ?? null;
+        $Employee_Cut = $Employee_Cut0;
+        $Location_ID = $Location_ID0;
         $Total = $_POST["Total_$i"] ?? null;
 
         $sql = "insert into sales (Inventory_ID, Sale_Date, Quantity, Discount, Price, Cut_ID, Customer_ID, Total_Price) values ('$Drug_ID', '$Date',
