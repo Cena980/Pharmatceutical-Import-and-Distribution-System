a23f317b27fe093e1fe4bb5c8c9fdaa7850c5896
Alisina Nazari <cena.nazari980@gmail.com>
2025-01-30 15:12:29 +0430

    stock issue solved for refilll report

--- Front End/php/refillreport.php
+++ Front End/php/refillreport.php
@@ -1,26 +1,25 @@
 <?php
 
     include 'connection.php';
-    $query = "SELECT * FROM main WHERE Amount_Left <= 0";
+    $query = "SELECT * FROM inventorystockstatus WHERE Remaining_Stock <= 0";
     $res = mysqli_query($connect, $query);
 
     $num_rows = mysqli_num_rows($res);
     if($num_rows>0){
         echo "<table border='1' id='tblreport'>";
         echo "<tr>
-                    <th>Inventory ID</th><th>Drug ID</th><th>Drug Name</th><th>Expiration</th><th>Cost</th>
-                    <th>Price</th><th>Initial_Amount</th><th>Amount Left</th>
+                    <th>Inventory ID</th><th>Drug ID</th>
+                    <th>Drug Name</th><th>Expiration</th>
+                    <th>Initial_Amount</th><th>Remaining Stock</th>
                 </tr>";
         while ($r = mysqli_fetch_assoc($res)) {
             echo "<tr>";
             echo "<td>" . $r['Inventory_ID'] . "</td>";
             echo "<td>" . $r['Drug_ID'] . "</td>";
-            echo "<td>" . $r['drug_name'] . "</td>";
+            echo "<td>" . $r['Drug_Name'] . "</td>";
             echo "<td>" . $r['Expiration'] . "</td>";
-            echo "<td>" . $r['Cost'] . "</td>";
-            echo "<td>" . $r['Price'] . "</td>";
             echo "<td>" . $r['Initial_Amount'] . "</td>";
-            echo "<td>" . $r['Amount_Left'] . "</td>";
+            echo "<td>" . $r['Remaining_Stock'] . "</td>";
             echo "</tr>";
         }
         echo "</table>";
