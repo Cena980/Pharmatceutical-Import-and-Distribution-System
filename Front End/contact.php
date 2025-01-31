1ca6c277bca76d479851a2c36dec00e8cbe9e7c0
Alisina Nazari <cena.nazari980@gmail.com>
2025-01-31 23:27:19 +0430

    photo added

--- Front End/contact.php
+++ Front End/contact.php
@@ -16,7 +16,7 @@
                 foreach($Shareholders as $Shareholder){
                     ?>
                     <div class="card">
-                        <img src="avatar.jpeg" alt="dp" style="width:100%">
+                    <img src="images/<?php echo $Shareholder['Holder_Name']; ?>.jpg" alt="dp" style="width:200px">
                         <div class="container_card">
                             <h3><b><?php echo $Shareholder['Holder_Name']?></b></h3>
                             <h4><b><?php echo $Shareholder['Phone']?></b></h4>
